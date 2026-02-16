77843aac86e9711a40373b7d8bf91db25c7a737e
ELETTRIC80\villarreal.e <villarreal.e@e80group.com>
2026-02-16 16:02:21 -0600

    Deploy website - based on e70c38a4c9d3852d1f9310bff703d1ed4f2acd5d

--- assets/js/5e5940fd.94fb67d3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[4609],{6638:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/DocE80/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.60779b9a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/DocE80/blog","blogTitle":"Blog","authorsListPath":"/DocE80/blog/authors"}')}}]);
